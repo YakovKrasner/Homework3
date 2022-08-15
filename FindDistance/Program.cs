b6b1cf12a2cc809d36eec15e9bd3dffa175ef686
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-15 14:10:10 +0700

    Find a Distance

---
+++ FindDistance/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите координату Х для точки А ");
+double coordinate_x_for_A = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите координату Y для точки A ");
+double coordinate_y_for_A = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите координату Z для точки А ");
+double coordinate_z_for_A = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Введите координату Х для точки B ");
+double coordinate_x_for_B = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите координату Y для точки B ");
+double coordinate_y_for_B = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Введите координату Z для точки B ");
+double coordinate_z_for_B = Convert.ToInt32 (Console.ReadLine());
+
+Console.WriteLine ("Расстояние между точками равно " + Math.Sqrt (Math.Pow((coordinate_x_for_B - coordinate_x_for_A), 2) + Math.Pow((coordinate_y_for_B - coordinate_y_for_A), 2) + Math.Pow((coordinate_z_for_B - coordinate_z_for_A), 2)));
