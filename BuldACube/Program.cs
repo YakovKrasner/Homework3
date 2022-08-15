e8863f8cc8d150068609da7aab4a002e6292efee
Yakov Krasner <yakov.krasner@yandex.ru>
2022-08-15 14:10:37 +0700

    Build A Cube

---
+++ BuldACube/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine ("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+    for (int count = 1; count <= number; count ++)
+        {
+            Console.WriteLine (Math.Pow (count,3));
+        }
